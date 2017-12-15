dcac0bacdab2d8b4a372075a42b757458b1d5a48
lushijie <lushijie1218@126.com>
2017-12-15 19:45:19 +0800

    add

--- test.js
+++
@@ -1,8 +0,0 @@
-/*
-* @Author: lushijie
-* @Date:   2017-11-25 16:25:06
-* @Last Modified by:   lushijie
-* @Last Modified time: 2017-11-25 16:31:25
-*/
-const Utils = require('./');
-console.log(Utils.isInt(123.00));
