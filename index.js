45a3a1c3eb4062c6426089e8983836a142266e5f
lushijie <lushijie03@meituan.com>
2018-11-14 10:22:11 +0800

    ci

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-// based on https://github.com/thinkjs/think-helper/blob/master/index.js
-
 const toString = Object.prototype.toString;
 const crypto = require('crypto');
 const uuid = require('uuid');
@@ -208,4 +206,100 @@ exports.md5 = function(str) {
 exports.uuid = function(version) {
   if (version === 'v1') return uuid.v1();
   return uuid.v4();
+}
+
+exports.interop = function(require) {
+  return require && require.__esModule ? require['default'] : require;
+}
+
+exports.dateTimeFormat = function(date, fmt) {
+  var o = {
+    'M+': date.getMonth() + 1, // 月份
+    'd+': date.getDate(), // 日
+    'h+': date.getHours(), // 小时
+    'm+': date.getMinutes(), // 分
+    's+': date.getSeconds(), // 秒
+    'q+': Math.floor((date.getMonth() + 3) / 3), // 季度
+    S: date.getMilliseconds(), // 毫秒
+  };
+  if (/(y+)/.test(fmt)) {
+    fmt = fmt.replace(
+      RegExp.$1,
+      (date.getFullYear() + '').substr(4 - RegExp.$1.length)
+    );
+  }
+  for (var k in o) {
+    if (new RegExp('(' + k + ')').test(fmt)) {
+      fmt = fmt.replace(
+        RegExp.$1,
+        RegExp.$1.length === 1
+          ? o[k]
+          : ('00' + o[k]).substr(('' + o[k]).length)
+      );
+    }
+  }
+  return fmt;
+}
+
+exports.renameKeys = function(object, changeMap, onlyGetMapItem) {
+  if (!changeMap || typeof changeMap !== 'object') {
+    return object;
+  }
+
+  if (Array.isArray(object)) {
+    const newArray = [];
+    for (let i = 0; i < object.length; i++) {
+      newArray.push(Utils.renameKeys(object[i], changeMap));
+    }
+    return newArray;
+  } else {
+    if (typeof object !== 'object') {
+      return object;
+    }
+    const newCopy = {};
+    const copy = Utils.extend({}, object);
+
+    for (const key in changeMap) {
+      if (typeof changeMap[key] === 'object') {
+        if (copy.hasOwnProperty(key)) {
+          copy[key] = Utils.renameKeys(copy[key], changeMap[key]);
+          newCopy[key] = Utils.renameKeys(copy[key], changeMap[key]);
+          continue;
+        }
+      }
+
+      if (copy.hasOwnProperty(key) && key !== changeMap[key]) {
+        const temp = copy[key];
+
+        copy[changeMap[key]] = temp;
+        newCopy[changeMap[key]] = temp;
+        delete copy[key];
+      }
+    }
+    return !onlyGetMapItem ? copy : newCopy;
+  }
+}
+
+// getDestructResult(['houseId', 'item:item1'], this.state);
+exports.getDestructResult = function (properties, sourceObject) {
+  const destObject = {};
+
+  const aliasMap = {};
+  properties = Array.isArray(properties) ? properties : [properties];
+
+  // 建立别名映衬
+  properties = properties.map(function(ele) {
+    if (ele.indexOf(':') > -1) {
+      aliasMap[ele.split(':')[0]] = ele.split(':')[1];
+    }
+    return ele.split(':')[0];
+  });
+
+  // 如果存在别名，新对象使用别名
+  Object.keys(sourceObject).forEach(function(ele) {
+    if (properties.indexOf(ele) > -1) {
+      destObject[aliasMap[ele] || ele] = sourceObject[ele];
+    }
+  });
+  return destObject;
 }
